82468ab9870d749c932a5a4b734ccf00c638c963
poorvakhandare3006 <poorvakhandare1999@gmail.com>
2020-04-23 15:48:07 +0530

    template inheritance added

--- shop/views.py
+++ shop/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'shop/index.html')
 def about(request):
-    return render(request,'shop/index.html')
+    return render(request,'shop/about.html')
 def contact(request):
     return render(request,'shop/index.html')
 def tracker(request):

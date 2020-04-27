d601fc0f448dc7ef7c17fadb9ffeb686517b92fc
poorvakhandare3006 <poorvakhandare1999@gmail.com>
2020-04-27 21:14:31 +0530

    contact page added

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Contact
 from math import ceil
 
 # Create your views here.
@@ -28,6 +28,14 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        #print(name)
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
     return render(request, 'shop/contact.html')
 
 def tracker(request):
